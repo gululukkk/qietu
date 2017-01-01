ebf65c7f734be030b6be739694ed1abb345f5045
huangyonglong <1297368547@qq.com>
2017-01-01 15:24:00 +0800

    图片轮播的圆点导航做好了

--- js/script.js
+++ js/script.js
@@ -3,13 +3,17 @@
 	var index = 0;
 	var fina_pic_index = $img_list.length - 1;
 	var $numbered = $('#part2 .pic-box .numbered');
+    $img_list.toggle_pic_display = function(numbered) {
+        $(this).eq(numbered).stop(true).fadeIn()
+            .siblings('img').stop('img').stop(true).fadeOut();
+		$numbered.text(index+1);
+    }
 	$('#part2 .left-arrow').click(function() {
 		index++;
 		if (index > fina_pic_index) {
 			index = 0;
 		}
-		$numbered.text(index);
-		$img_list.eq(index).stop(true).fadeIn().siblings('img').stop(true).fadeOut();
+        $img_list.toggle_pic_display(index);
 	});
 
 	$('#part2 .right-arrow').click(function () {
@@ -17,7 +21,14 @@
 		if (index < 0) {
 			index = fina_pic_index;
 		}
-		$numbered.text(index);
-		$img_list.eq(index).stop(true).fadeIn().siblings('img').stop(true).fadeOut();
+		$img_list.toggle_pic_display(index);
 	});
+    
+    var $dotted_list = $('#part2 .dotted-list .item');
+    $dotted_list.click(function() {
+        $(this).css('background','#c0a057')
+            .siblings('.item').css('background','#5b5b5b');
+        var index = $(this).index();
+    	$img_list.toggle_pic_display(index);
+    });
 })();
